79e8f1988aef4345a9a0b0b267a12eb8e1f12a68
qq1327185450 <1327185450@qq.com>
2022-10-05 22:01:17 +0800

    fix: 修改test为text

--- gsw/1.py
+++ gsw/1.py
@@ -9,7 +9,7 @@ for i in range(1,21):
   ex='<a style=" float:left;" .*? href="(.*?)">(.*?)</a>.*?span.*?<a.*? href=.*?>(.*?)</a.*?>'
   text=re.findall(ex,page_text,re.S)
   for item in text:
-    with open("test.txt", "a",encoding='utf-8') as fs:  
+    with open("text.txt", "a",encoding='utf-8') as fs:  
       fs.write(str(item[1])+'  '+str(item[2])+'  '+'https://so.gushiwen.cn/'+str(item[0])+'\n')  
       # fs.close()
 # <a style=" float:left;" target="_blank" href="/mingju/juv_56cd8fe503c4.aspx">十年寒窗无 
